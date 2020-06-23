d588665debd5cf5e18316a4224ff6705217bdd57
Callum McColl <callum.mccoll1@gmail.com>
2020-06-24 01:42:14 +1000

    Added some attributes

--- conversions.h
+++ conversions.h
@@ -73,29 +73,29 @@ extern "C" {
 #endif
 
 // Converting between image coordinate systems.
-gu_pixel_coordinate cam_coord_to_px_coord(const gu_camera_coordinate);
-gu_camera_coordinate px_coord_to_cam_coord(const gu_pixel_coordinate);
-gu_percent_coordinate px_coord_to_pct_coord(const gu_pixel_coordinate);
-gu_pixel_coordinate pct_coord_to_px_coord(const gu_percent_coordinate, const pixels_u, const pixels_u);
+gu_pixel_coordinate cam_coord_to_px_coord(const gu_camera_coordinate) __attribute__((const));
+gu_camera_coordinate px_coord_to_cam_coord(const gu_pixel_coordinate) __attribute__((const));
+gu_percent_coordinate px_coord_to_pct_coord(const gu_pixel_coordinate) __attribute__((const));
+gu_pixel_coordinate pct_coord_to_px_coord(const gu_percent_coordinate, const pixels_u, const pixels_u) __attribute__((const));
 
 // Converting from image coordinate systems to the robot relative coordiante system.
-bool px_coord_to_rr_coord(const gu_pixel_coordinate, const gu_robot, gu_relative_coordinate *, const int);
-bool pct_coord_to_rr_coord(const gu_percent_coordinate, const gu_robot, gu_relative_coordinate *, const int);
+bool px_coord_to_rr_coord(const gu_pixel_coordinate, const gu_robot, gu_relative_coordinate *, const int) __attribute__((pure, nonnull));
+bool pct_coord_to_rr_coord(const gu_percent_coordinate, const gu_robot, gu_relative_coordinate *, const int) __attribute__ ((pure, nonnull));
 
 // Converting from the robot relative coordinate system to the image coordinate systems.
-bool rr_coord_to_pct_coord(const gu_relative_coordinate, const gu_robot, const int, gu_percent_coordinate *);
-bool rr_coord_to_px_coord(const gu_relative_coordinate, const gu_robot, const int, gu_pixel_coordinate *, pixels_u, pixels_u);
+bool rr_coord_to_pct_coord(const gu_relative_coordinate, const gu_robot, const int, gu_percent_coordinate *) __attribute__((pure, nonnull));
+bool rr_coord_to_px_coord(const gu_relative_coordinate, const gu_robot, const int, gu_pixel_coordinate *, pixels_u, pixels_u) __attribute__((pure, nonnull));
 
 // Converting from the robot relative coordinate system to the field coordinate systems.
-gu_cartesian_coordinate rr_coord_to_cartesian_coord(const gu_relative_coordinate);
-gu_cartesian_coordinate rr_coord_to_cartesian_coord_from_source(const gu_relative_coordinate, const gu_cartesian_coordinate);
-gu_field_coordinate rr_coord_to_field_coord(const gu_relative_coordinate, const degrees_t);
-gu_field_coordinate rr_coord_to_field_coord_from_source(const gu_relative_coordinate, const gu_field_coordinate, const degrees_t);
+gu_cartesian_coordinate rr_coord_to_cartesian_coord(const gu_relative_coordinate) __attribute__((const));
+gu_cartesian_coordinate rr_coord_to_cartesian_coord_from_source(const gu_relative_coordinate, const gu_cartesian_coordinate) __attribute__((const));
+gu_field_coordinate rr_coord_to_field_coord(const gu_relative_coordinate, const degrees_t) __attribute__((const));
+gu_field_coordinate rr_coord_to_field_coord_from_source(const gu_relative_coordinate, const gu_field_coordinate, const degrees_t) __attribute__((const));
 
 // Converting from field coordinates to relative coordinates.
-gu_relative_coordinate cartesian_coord_to_rr_coord(const gu_cartesian_coordinate);
-gu_relative_coordinate cartesian_coord_to_rr_coord_from_source(const gu_cartesian_coordinate, const gu_cartesian_coordinate);
-gu_relative_coordinate field_coord_to_rr_coord_to_target(const gu_field_coordinate, const gu_cartesian_coordinate);
+gu_relative_coordinate cartesian_coord_to_rr_coord(const gu_cartesian_coordinate) __attribute__((const));
+gu_relative_coordinate cartesian_coord_to_rr_coord_from_source(const gu_cartesian_coordinate, const gu_cartesian_coordinate) __attribute__((const));
+gu_relative_coordinate field_coord_to_rr_coord_to_target(const gu_field_coordinate, const gu_cartesian_coordinate) __attribute__((const));
 
 #ifdef __cplusplus
 }
