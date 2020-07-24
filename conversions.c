99049eb391d1962ccfc89d1c794c76c89d2ef2f3
Morgan McColl <morgan.mccoll92@gmail.com>
2020-07-23 22:53:17 -0400

    Added #def for M_PI_2

--- conversions.c
+++ conversions.c
@@ -58,8 +58,14 @@
 
 #include "conversions.h"
 #include "hidden_conversions.h"
-
 #include <math.h>
+
+/*#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif*/
+#ifndef M_PI_2
+    #define M_PI_2 1.57079632679489661923
+#endif
 
 gu_pixel_coordinate cam_coord_to_px_coord(const gu_camera_coordinate coord)
 {
